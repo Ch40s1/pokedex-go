c739e0691762848af77128aeb9de1bff4f7daef3
Ch40s1 <114247747+Ch40s1@users.noreply.github.com>
2024-08-12 20:51:46 -0500

    added final api commands

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 )
 
 func main() {
-	pokeClient := pokeapi.NewClient(5 * time.Second)
+	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
 	cfg := &config{
+		caughtPokemon: map[string]pokeapi.Pokemon{},
 		pokeapiClient: pokeClient,
 	}
-	// starts the cli
+
 	startRepl(cfg)
 }
